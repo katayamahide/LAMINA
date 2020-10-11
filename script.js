19d627ae654ca7c1a24951f6e3b77955a705ff3e
ohata <ohata@ohatanoMacBook-Pro.local>
2020-10-11 12:49:15 +0900

    new

---
+++ script.js
@@ -0,0 +1,9 @@
+$(function(){
+  $('.slider').slick({
+    autoplay:true,
+    autoplaySpeed:3000,
+    dots:true,
+    arrows:true,
+    prevArrow:'<button type="button" class="slick-prev">Previous</button>',
+  });
+});
